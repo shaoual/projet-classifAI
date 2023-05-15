afe1fb1204300a049ae658518bbde03800778a34
pierre pracht <pierre.pracht@gmail.com>
2023-05-15 21:48:11 +0200

    page 404

---
+++ src/pages/[...page404].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+//optionnel
+const props = defineProps<{ page404: string }>()
+console.log(props.page404)
+</script>
+<template>
+  <div>
+    <h1 class="text-xl">Pas trouvé : {{ $route.fullPath }}</h1>
+  </div>
+</template>
