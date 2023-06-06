3d0af8cc06082c73d78b97f64cfbe3a842c41080
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 16:26:38 +0200

    améliorations footer (vrai)

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,59 +1,113 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import FacebookIcon from '@/components/icons/IconFacebook.vue'
+import InstagramIcon from '@/components/icons/IconInstagram.vue'
+import logo from '@/components/logo.vue'
 </script>
 
 <template>
+<!-- Footer pour les appareils mobiles -->
+<footer class="footer-gradient p-9 lg:hidden">
+<div class="space-y-3">
+<h4 class="uppercase font-bold">
+<logo class="hidden lg:block mr-2" /> ClassifAI
+</h4>
 
-    <section>
-      
-    <div class="flex items-center justify-center mt-20 md:grid md:grid-cols-2 md:mt-32 lg:mt-62">
+<div class="flex justify-between ">
+<div>
+<ul class="flex items-center gap-4">
+<li>
+<a href="https://facebook.com" aria-label="facebook">
+<FacebookIcon />
+</a>
+</li>
+<li>
+<a href="https://instagram.com" aria-label="instagram">
+<InstagramIcon />
+</a>
+</li>
+</ul>
+</div>
+<nav class="text-right">
+<ul>
+<li>
+<RouterLink class="block rounded-lg" to="/politique">Politique de confidentialité</RouterLink>
+</li>
+<li>
+<RouterLink class="block mt-2 rounded-lg" to="/cookie">Cookies</RouterLink>
+</li>
+<li>
+<RouterLink class="block mt-2 rounded-lg" to="/contact">Contact</RouterLink>
+</li>
+<li>
+<RouterLink class="block mt-2 rounded-lg" to="/apropos">À propos</RouterLink>
+</li>
+</ul>
+</nav>
+</div>
+</div>
 
- <div class="hidden md:block order-1 md:order-2 mx-auto">
-                
-                <div class="flex justify-center">
-                  <img src="/Projet_web/ia_illustration_1.png"
-                    class="w-2/3 md:full lg:1/3" alt="illustration" />
-                </div>
-
-                </div>
+<div class="mt-4">
+<p class="text-sm text-center">Tous droits réservés © 2023 - ClassifAI</p>
+</div>
+</footer>
 
 
+<!-- Footer pour les tablettes et les écrans plus grands -->
+<footer class="footer-gradient p-9 hidden lg:grid lg:grid-cols-4">
+<div>
+<h4 class="uppercase font-bold flex items-center">
+<logo class="hidden lg:block mr-2" /> ClassifAI
+</h4>
+</div>
+
+<div>
+<ul class="grid grid-cols-1 gap-2">
+<li>
+<RouterLink class="block rounded-lg active" to="/politique">Politique de confidentialité</RouterLink>
+</li>
+<li>
+<RouterLink class="block mt-2 rounded-lg" to="/cookie">Cookies</RouterLink>
+</li>
+</ul>
+</div>
+
+<div>
+<ul class="grid grid-cols-1 gap-2">
+<li>
+<RouterLink class="block rounded-lg" to="/contact">Contact</RouterLink>
+</li>
+<li>
+<RouterLink class="block mt-2 rounded-lg" to="/apropos">À propos</RouterLink>
+</li>
+</ul>
+</div>
+
+<div class="flex items-center justify-end">
+<ul class="flex gap-4">
+<li>
+<a href="https://facebook.com" aria-label="facebook">
+<FacebookIcon />
+</a>
+</li>
+<li>
+<a href="https://instagram.com" aria-label="instagram">
+<InstagramIcon />
+</a>
+</li>
+</ul>
+</div>
+
+<div class="col-span-4 mt-8">
+<p class="text-sm text-center">Tous droits réservés © 2023 - ClassifAI</p>
+</div>
+</footer>
 
 
+</template>
 
-  <div class="order-2 md:order-1 mx-12 md:mx-22 lg:mx-40">              
-  <div class="lg:flex">
-    <h1 class="text-4xl text-center md:text-justify font-Helvetica font-bold mt-3 text-white">ClassifAI</h1>
-  </div>
 
-  <div class="lg:flex text-center md:text-justify mt-5 font-bold text-white  ">
-    <p >Open your AI’s</p>
-  </div>
-
-      <div class="order-3 md:hidden mt-20 mx-auto">
-        <div class="flex justify-center">
-          <img src="/Projet_web/ia_illustration_1.png" class="w-2/3 md:w-full" alt="illustration" />
-        </div>
-        </div>
-
-  <div class="lg:flex mt-6 text-white text-center md:text-left mx-4 md:mx-0">
-    <p>ClassifAI est un site web conçu pour aider les personnes intéressées par l'intelligence artificielle, à mieux comprendre et naviguer dans le domaine de l'IA.</p>
-  </div>
-
-<div class="flex justify-center md:float-left">
-      <button class="bg-purple-900 hover:bg-gray-400 text-white mt-10 py-4 px-14 rounded inline-flex items-center border-2 border-whiter lg:flex-row">
-        <RouterLink to="/DécouvrirlesIA">Découvrir les IA</RouterLink>
-      </button>
-    </div>
-
-  </div>
-
-    </div>  
- 
-
-      
-      <!-- Jumbotron -->
-    </section>
-    <!-- Section: Design Block -->
-
-    </template>
+<style>
+.footer-gradient {
+background: linear-gradient(to bottom, rgba(140, 102, 165, 0.2), rgba(179, 178, 193, 0.2));
+}
+</style>
