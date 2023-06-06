98ec67989d2d9cef1ceb28a3d60288328f7cd2d2
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 23:17:51 +0200

    amelioration inscription + email.vue

--- src/pages/Inscription.vue
+++ src/pages/Inscription.vue
@@ -1,51 +1,46 @@
-<script setup>
-import PocketBase from 'pocketbase'
-import { ref } from 'vue';
-</script>
-
 <template>
   <main class="main-content">
     <h2 class="text-center">S'INSCRIRE</h2>
    
     <form class="space-y-6 mt-10 mb-36 sm:mx-auto sm:w-full sm:max-w-sm">
       <div class="mb-2 md:mb-8">
-        <label class="block tracking-wide text-Jet text-xs mb-2" for="last-name">
+        <label class="block tracking-wide  text-xs mb-2" for="last-name">
           Nom
         </label>
-        <input class="block w-full bg-Impure-white text-Jet border rounded py-3 px-4 mb-3 h-11" id="last-name" type="text">
+        <input class="block w-full bg-Impure-white text-black border rounded py-3 px-4 mb-3 h-11" id="last-name" type="text">
       </div>
       <div class="mb-2 md:mb-8">
-        <label class="block tracking-wide text-Jet text-xs mb-2" for="first-name">
+        <label class="block tracking-wide  text-xs mb-2" for="first-name">
           Prénom
         </label>
-        <input class="block w-full bg-Impure-white text-Jet border rounded py-3 px-4 mb-3 h-11" id="first-name" type="text">
+        <input class="block w-full bg-Impure-white text-black border rounded py-3 px-4 mb-3 h-11" id="first-name" type="text">
       </div>
       <div class="mb-2 md:mb-8">
-        <label class="block tracking-wide text-Jet text-xs mb-2" for="login">
+        <label class="block tracking-wide  text-xs mb-2" for="login">
           E-mail
         </label>
-        <input class="block w-full bg-Impure-white text-Jet border rounded py-3 px-4 mb-3 h-11" id="login" type="email">
+        <input class="block w-full bg-Impure-white text-black border rounded py-3 px-4 mb-3 h-11" id="login" type="email">
       </div>
       <div class="mb-2 md:mb-8">
-        <label class="block tracking-wide text-Jet text-xs mb-2" for="passwd">
+        <label class="block tracking-wide  text-xs mb-2" for="passwd">
           Mot de passe
         </label>
-        <input class="block w-full bg-Impure-white text-Jet border rounded py-3 px-4 mb-3 h-11" id="passwd" type="password">
+        <input class="block w-full bg-Impure-white text-black border rounded py-3 px-4 mb-3 h-11" id="passwd" type="password">
       </div>
       <div class="mb-2 md:mb-8">
-        <label class="block tracking-wide text-Jet text-xs mb-2" for="passwd-confirm">
+        <label class="block tracking-wide  text-xs mb-2" for="passwd-confirm">
           Confirmer le mot de passe
         </label>
-        <input class="block w-full bg-Impure-white text-Jet border rounded py-3 px-4 mb-3 h-11" id="passwd-confirm" type="password">
+        <input class="block w-full bg-Impure-white text-black border rounded py-3 px-4 mb-3 h-11" id="passwd-confirm" type="password">
       </div>
       <div class="flex items-center justify-center">
-        <input type="checkbox" id="conditions" name="conditions" class="mr-2 purple-checkbox">
+        <input type="checkbox" id="conditions" name="conditions" class="mr-2 purple-checkbox" v-model="acceptedConditions">
         <router-link to="/conditions">
           <label for="conditions">J'accepte les conditions d'utilisation</label>
         </router-link>
       </div>
       <div class="md:col-span-2 flex justify-center m-4">
-        <button class="bg-purple-500 text-white font-bold rounded py-2 px-10 mb-5" type="button" @click="register">S'inscrire</button>
+        <button class="bg-purple-500 text-white font-bold rounded py-2 px-10 mb-5" type="button" @click="register" :disabled="!acceptedConditions">S'inscrire</button>
       </div>
       <router-link to="/seconnecter">
         <p class="text-center">Déjà un compte ? <span class="text-violet-500">Se connecter</span></p>
@@ -54,22 +49,37 @@ import { ref } from 'vue';
   </main>
 </template>
 
-
-
 <script>
-const pb = new PocketBase('http://127.0.0.1:8090');
-
-const register = async () => {
-  const email = document.getElementById('login').value;
-  const password = document.getElementById('passwd').value;
-  const passwordConfirm = document.getElementById('passwd-confirm').value;
-  const name = document.getElementById('last-name').value;
-
-  await pb.collection('users').create({ email, password, passwordConfirm, name });
-};
+import PocketBase from 'pocketbase';
+import { ref } from 'vue';
 
 export default {
-  register,
+  setup() {
+    const pb = new PocketBase('http://127.0.0.1:8090');
+    const acceptedConditions = ref(false);
+
+    const register = async () => {
+      if (!acceptedConditions.value) {
+        // L'utilisateur n'a pas accepté les conditions
+        return;
+      }
+
+      const email = document.getElementById('login').value;
+      const password = document.getElementById('passwd').value;
+      const passwordConfirm = document.getElementById('passwd-confirm').value;
+      const name = document.getElementById('last-name').value;
+
+      await pb.collection('users').create({ email, password, passwordConfirm, name });
+    };
+
+    return {
+      acceptedConditions,
+      register,
+    };
+  },
 };
 </script>
 
+<style>
+/* Vos styles CSS ici */
+</style>
---
+++ src/pages/email.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="main-content h-screen">
+
+        <p class="mt-40 mb-5 w-2/4">Veuillez saisir l'adresse e-mail associée à votre compte d'utilisateur. 
+            Un mail de vérification vous sera adressé. Lorsque vous le recevrez, vous pourrez choisir un nouveau mot de passe</p>
+
+        <form class="flex flex-col md:flex-row mb-4 items-end gap-4">
+            <div>
+                <label class="block tracking-wide mb-2" for="grid-email">E-mail</label>
+                <input class="block text-black border rounded py-3 px-4 h-10" id="login" type="email">
+            </div>
+            <div>
+                <button type="submit" class="px-4 py-2 bg-violet-900 text-white font-bold rounded focus:outline-none">Envoyer</button>
+            </div>
+        </form>
+    </main>
+</template>
