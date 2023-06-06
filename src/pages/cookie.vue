ea54df6eed43159b8eff866653796ea7f3e2175e
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 12:04:49 +0200

    amélioration cookies

--- src/pages/cookie.vue
+++ src/pages/cookie.vue
@@ -1,5 +1,3 @@
-
-
 <script setup lang="ts">
 import { ref } from 'vue'
 import { RouterView } from 'vue-router'
@@ -10,53 +8,38 @@ import { RouterLink } from 'vue-router'
 
 <template> 
 
-<h1 class="text-3xl m-[60px] font-bold text-white">COOKIES</h1>
+<main class="main-content">
+
+<h2>COOKIES</h2>
+
+    <div class="lg:w-1/2 mb-20">
+        <p class="mt-6">ClassifAI utilise des cookies et des technologies similaires sur son site web. Cette politique de cookies explique ce que sont les cookies, comment nous les utilisons et quels sont vos choix concernant leur utilisation.</p>
+
+        <p class="mt-6">Qu'est-ce qu'un cookie ?</p>
+
+        <p class="mt-6">Un cookie est un petit fichier texte qui est stocké sur votre ordinateur ou votre appareil mobile lorsque vous visitez un site web. Les cookies sont largement utilisés pour permettre aux sites web de fonctionner efficacement, pour collecter des informations sur les préférences des utilisateurs et pour améliorer l'expérience de navigation.</p>
+
+        <p class="mt-6">Comment utilisons-nous les cookies ? </p>
+
+        <p class="mt-6">Nous utilisons des cookies pour diverses raisons, notamment pour fournir des fonctionnalités au site et améliorer votre expérience de navigation ; comprendre comment les utilisateurs interagissent avec le site et améliorer sa convivialité pour personnaliser le contenu et les publicités que vous voyez sur le site et sur d'autres sites web.</p>
+
+        <p class="mt-6">Types de cookies que nous utilisons Nous utilisons les types de cookies suivants sur notre site :</p>
+
+        <ul>
+            <li>Cookies de session : ces cookies sont temporaires et sont supprimés lorsque vous fermez votre navigateur web ou que vous vous déconnectez de notre site.</li>
+            <li>Cookies persistants : Ces cookies restent sur votre ordinateur ou votre appareil mobile après la fermeture de votre navigateur web ou la déconnexion de notre Site. Nous utilisons des cookies persistants pour comprendre comment les utilisateurs interagissent avec le site et pour améliorer sa convivialité.</li>
+            <li>Cookies de tiers : Nous utilisons des cookies de tiers, tels que Google Analytics, pour suivre l'utilisation du site et améliorer notre marketing. Ces cookies peuvent être utilisés par ces tiers pour vous montrer des publicités pertinentes sur d'autres sites web.</li>
+        </ul>
 
 
-<p class="text-sm  m-[60px] text-white">ClassifAI utilise des cookies et des technologies similaires sur son site web. Cette politique de 
-cookies explique ce que sont les cookies, comment nous les utilisons et quels sont vos choix concernant leur utilisation.</p>
-<p class="text-sm  mb-[-45px] m-[60px] text-white">
-Qu'est-ce qu'un cookie ? </p>
-<p class="text-sm  mb-[-45px] m-[60px] text-white">
-Un cookie est un petit fichier texte qui est stocké sur votre ordinateur ou votre appareil mobile lorsque vous 
-visitez un site web. Les cookies sont largement utilisés pour permettre aux sites web de fonctionner efficacement, 
-pour collecter des informations sur les préférences des utilisateurs et pour améliorer l'expérience de navigation.
-</p><p class="text-sm  mb-[-45px] m-[60px] text-white">
-Comment utilisons-nous les cookies ? </p>
-<p class="text-sm  mb-[-45px] m-[60px] text-white">
-Nous utilisons des cookies pour diverses raisons, notamment pour
-pour fournir des fonctionnalités au site et améliorer votre expérience de navigation ; comprendre comment les 
-utilisateurs interagissent avec le site et améliorer sa convivialité pour personnaliser le contenu et les publicités 
-que vous voyez sur le site et sur d'autres sites web.</p>
-<p class="text-sm  mb-[-45px] m-[60px] text-white">
-Types de cookies que nous utilisons Nous utilisons les types de cookies suivants sur notre site :</p>
-<p class="text-sm  mb-8 m-[60px] text-white">
-Cookies de session : ces cookies sont temporaires et sont supprimés lorsque vous fermez votre navigateur web 
-ou que vous vous déconnectez de notre site.</p>
-<p class="text-sm  mb-8 m-[60px] text-white">
-Cookies persistants : Ces cookies restent sur votre ordinateur ou votre appareil mobile après la fermeture de 
-votre navigateur web ou la déconnexion de notre Site. Nous utilisons des cookies persistants pour comprendre comment 
-les utilisateurs interagissent avec le site et pour améliorer sa convivialité.</p>
-<p class="text-sm  mb-8 m-[60px] text-white">
-Cookies de tiers : Nous utilisons des cookies de tiers, tels que Google Analytics, pour suivre l'utilisation du 
-site et améliorer notre marketing. Ces cookies peuvent être utilisés par ces tiers pour vous montrer des publicités 
-pertinentes sur d'autres sites web.</p>
-<p class="text-sm  mb-8 m-[60px] text-white">
-Vos choix concernant les cookies Vous avez le choix d'accepter ou de refuser les cookies. Vous pouvez modifier 
-vos paramètres de cookies à tout moment en utilisant les paramètres de votre navigateur web. 
-Toutefois, si vous désactivez les cookies, certaines fonctionnalités du site risquent de ne pas fonctionner correctement.</p>
-<p class="text-sm  mb-8 m-[60px] text-white">
-Nous utilisons également des technologies de suivi, telles que les balises web, pour collecter des informations sur 
-l'utilisation de notre site. Ces technologies de suivi sont incluses dans nos courriels de marketing. Si vous ne souhaitez 
-pas que nous utilisions des technologies de suivi dans nos courriers électroniques de marketing, vous pouvez vous désabonner 
-en utilisant le lien de désabonnement fourni dans chaque courrier électronique.</p>
-<p class="text-sm  mb-8 m-[60px] text-white">
-Modifications de la présente politique en matière de cookies Nous pouvons mettre à jour la présente politique en 
-matière de cookies de temps à autre en publiant une nouvelle version sur notre site. Nous vous recommandons de consulter 
-régulièrement cette politique en matière de cookies afin de prendre connaissance de tout changement.
-</p>
+        <p class="mt-6">Vos choix concernant les cookies Vous avez le choix d'accepter ou de refuser les cookies. Vous pouvez modifier vos paramètres de cookies à tout moment en utilisant les paramètres de votre navigateur web. 
+        Toutefois, si vous désactivez les cookies, certaines fonctionnalités du site risquent de ne pas fonctionner correctement.</p>
 
+        <p class="mt-6">Nous utilisons également des technologies de suivi, telles que les balises web, pour collecter des informations sur l'utilisation de notre site. Ces technologies de suivi sont incluses dans nos courriels de marketing. Si vous ne souhaitez pas que nous utilisions des technologies de suivi dans nos courriers électroniques de marketing, vous pouvez vous désabonner en utilisant le lien de désabonnement fourni dans chaque courrier électronique.</p>
 
-<RouterView />
+        <p class="mt-6">Modifications de la présente politique en matière de cookies Nous pouvons mettre à jour la présente politique en matière de cookies de temps à autre en publiant une nouvelle version sur notre site. Nous vous recommandons de consulter régulièrement cette politique en matière de cookies afin de prendre connaissance de tout changement.</p>
 
+    </div>
+
+</main>
 </template>
