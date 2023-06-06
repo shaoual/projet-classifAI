aef32d53eee3059cd71278fc817f039b31aa98ca
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 23:43:15 +0200

    import icones card

---
+++ src/components/icons/Iconview.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.5834 7.34595H6.00002C5.51379 7.34595 5.04747 7.5391 4.70366 7.88292C4.35984 8.22673 4.16669 8.69305 4.16669 9.17928V17.4293C4.16669 17.9155 4.35984 18.3818 4.70366 18.7256C5.04747 19.0695 5.51379 19.2626 6.00002 19.2626H14.25C14.7363 19.2626 15.2026 19.0695 15.5464 18.7256C15.8902 18.3818 16.0834 17.9155 16.0834 17.4293V12.8459" stroke="#FBFBFB" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M9.66669 13.7626L18.8334 4.59595" stroke="#FBFBFB" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M14.25 4.59595H18.8333V9.17928" stroke="#FBFBFB" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+</template>
