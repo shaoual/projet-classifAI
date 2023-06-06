4720388659d3150e705e10a60df2e221fd462591
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 12:25:30 +0200

    amélioration page seconnecter

--- src/pages/seconnecter.vue
+++ src/pages/seconnecter.vue
@@ -3,15 +3,13 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">SE CONNECTER</h2>
-      </div>
-  
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#" method="POST">
-  
+  <main class="main-content">
+
+
+        <h2 class="text-center">SE CONNECTER</h2>
+
+        <form class="space-y-6 mt-10 mb-36 sm:mx-auto sm:w-full sm:max-w-sm" action="#" method="POST">
+
           <div>
             <label for="email" class="block text-sm  leading-6 text-white">E-mail </label>
             <div class="mt-2">
@@ -19,6 +17,7 @@ import { RouterView } from 'vue-router'
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             </div>
           </div>
+
           <div>
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm leading-6 text-white">Mot de passe </label>
@@ -27,29 +26,58 @@ import { RouterView } from 'vue-router'
             <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
- <p class="mt-10 text-center text-white text-sm ">
-        J'ai oublié mon mot de passe
 
-      </p>
+          <p class="mt-10 text-center text-white text-sm ">J'ai oublié mon mot de passe</p>
+
         <div class="flex justify-center space-x-4">
-  <button class="bg-gray-300 hover:bg-gray-400 text-gray-800  py-2 px-10 rounded inline-flex items-center lg:flex-row">
-    <span>S'inscrire</span>
-  </button>
+            <RouterLink to="/inscription"><button class="bg-white text-black font-bold py-2 px-5 rounded inline-flex items-center lg:flex-row">S'inscrire</button></RouterLink>
+            <RouterLink to="/profil"><button class="bg-white text-black font-bold py-2 px-5 rounded inline-flex items-center lg:flex-row">Se connecter</button></RouterLink>
+        </div>
 
-  <div class="w-1"></div> 
-
-  <button class="bg-gray-300 hover:bg-gray-400 text-gray-800  py-2 px-10 rounded inline-flex items-center lg:flex-row">
-    <span>Se connecter</span>
-  </button>
-</div>
-  
-          
-  
         </form>
-       
-        
+
+  </main>
+  </template>
+proposer.vue :
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
+
+</script>
+
+
+<template> 
+   <main class="main-content">
+
+
+    <h2 class="text-center lg:text-left">PROPOSER UNE IA</h2>
+
+    <form class="w-full lg:w-1/2 space-y-6 mt-10 mb-44" action="#" method="POST">
+      <div>
+        <label for="nom">Nom de l'IA</label>
+        <input type="text" id="nom" name="nom" class="w-full text-black rounded-md p-2">
       </div>
-    </div>
-    <RouterView />
+
+      <div>
+        <label for="url">L'URL du site</label>
+        <input type="text" id="url" name="url" class="w-full text-black rounded-md p-2">
+      </div>
+
+      <div>
+        <label for="description">Description de l'IA</label>
+        <textarea id="description" name="description" class="w-full text-black h-52 rounded-md p-2"></textarea>
+      </div>
+
+      <button class="float-right bg-white text-black font-bold py-2 px-10 inline-flex items-center">Envoyer</button>
+    </form>
+
+
+</main>
+
+<RouterView />
+
+
+
   </template>
   
