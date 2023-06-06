f06a9760b4968d4532be7ed1f5ff10eaecdfe7f9
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 14:44:58 +0200

    suppression de sous dossier

--- src/pages/sous-dossier/[nomParam].vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-defineProps<{ nomParam: string }>()
-</script>
-<template>
-  <p>Applé avec param : {{ nomParam }}</p>
-</template>
--- src/pages/sous-dossier/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <p>Contenu sous dossier</p>
-</template>
