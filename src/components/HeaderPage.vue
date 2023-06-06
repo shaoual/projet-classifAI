4620a42ab27864d2e1dc107f0a485c8a5ef924a2
Claude2003 <127388335+Claude2003@users.noreply.github.com>
2023-06-06 11:37:57 +0200

    commit

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,60 +1,52 @@
-<script setup lang="ts">
+
+  <script setup lang="ts">
 import { ref } from 'vue'
 import logo from '@/components/logo.vue'
 import drapeau from '@/components/icons/drapeau.vue'
 import icone_soleil from '@/components/icons/icone_soleil.vue'
 import { RouterLink } from 'vue-router'
 
-const menuIsOpen = ref(false)
+const activeMenu = ref(false)
+
+function closeMenu() {
+  activeMenu.value = false
+}
+
 </script>
 <template>
-  <header class=" bg-gray-900 flex items-center justify-between px-4 h-20 border-b-2 border-b-fond-site ">
+  <header class=" bg-fond-site flex items-center justify-between px-4 h-20 ">
   
 
     <RouterLink to="/">
       <logo/>
     </RouterLink>
-    <button
-      class="w-6 h-3 flex flex-col justify-between lg:hidden"
-      @pointerdown="menuIsOpen = !menuIsOpen"
-    >
+    <div class="relative z-10">
+      <button
+    class="w-6 h-3 flex flex-col justify-between lg:hidden"
+    @click="activeMenu = !activeMenu">
+
     <span
-        class="block bg-white h-[2px] w-full rounded-full transition duration-300"
-        :class="{ '-translate-y-[5px] ': menuIsOpen }"
-      ></span>
-      <span
-        class="block bg-white h-[2px] w-full rounded-full transition duration-300"
-        :class="{ '-translate-y-[5px] ': menuIsOpen }"
-      ></span>
-      <span
-        class="block bg-white h-[2px] w-full rounded-full transition duration-300"
-        :class="{ '-translate-y-[5px] ': menuIsOpen }"
-      ></span>
-    </button>
-   
-    <nav class="invisible bg-gray-900 fixed inset-0 mt-20 px-6 py-12 flex flex-col justify-between lg:visible lg:opacity-100 lg:static lg:bg-transparent lg:mt-0 lg:flex-row lg:flex-1" :class="{ '!visible !opacity-100': menuIsOpen }">
+      class="block h-[2px] w-full rounded-full transition duration-300"
+      :class="{ 'bg-white': !activeMenu, 'bg-white translate-y-[5px] rotate-45': activeMenu } "></span>
+    <span
+      class="block h-[2px] w-full rounded-full transition duration-300"
+      :class="{ 'bg-white': !activeMenu, 'bg-white -translate-y-[5px] -rotate-45': activeMenu }"></span>
+  </button>
+   </div>
+
+
+    <nav class="invisible bg-gray-900 fixed inset-0 mt-20 px-6 py-12 flex flex-col justify-between lg:visible lg:opacity-100 lg:static lg:bg-transparent lg:mt-0 lg:flex-row lg:flex-1" :class="{ '!visible !opacity-100': activeMenu }">
  
-
-
-    
-      <nav class="invisible bg-gray-900 fixed inset-0 mt-20 px-6 py-12 flex flex-col justify-between lg:visible lg:opacity-100 lg:static lg:bg-transparent lg:mt-0 lg:flex-row" :class="{ '!visible !opacity-100': menuIsOpen }">
+  <nav class="invisible bg-gray-900 fixed inset-0 mt-20 px-6 py-12 flex flex-col justify-between lg:visible lg:opacity-100 lg:static lg:bg-transparent lg:mt-0 lg:flex-row" :class="{ '!visible !opacity-100': activeMenu }">
   <ul class="flex flex-col gap-4 lg:flex-row">
-    <li>
-      <RouterLink class="text-gray-500 p-4 block rounded-lg active" to="/Découvrir les IA">Découvrir les IA</RouterLink>
-    </li>
-    <hr class="border-gray-500">
-    <li>
-      <RouterLink class="text-gray-500 p-4 block rounded-lg" to="/Proposer">Proposer une IA</RouterLink>
-    </li>
-    <hr class="border-gray-500">
-    <li>
-      <RouterLink class="text-gray-500 p-4 block rounded-lg" to="/Contact">Contact</RouterLink>
-    </li>
-    <hr class="border-gray-500">
+    <li><RouterLink class="text-light-gray p-4 block rounded-lg active" to="/Découvrir les IA">Découvrir les IA</RouterLink></li>
+    <hr class="border-light-gray">
+    <li><RouterLink class="text-light-gray p-4 block rounded-lg" to="/Proposer">Proposer une IA</RouterLink></li>
+    <hr class="border-light-gray">
+    <li><RouterLink class="text-light-gray p-4 block rounded-lg" to="/Contact">Contact</RouterLink></li>
+    <hr class="border-light-gray">
    
-    <li class="lg:hidden">
-      <RouterLink class="text-gray-500 p-4 block rounded-lg" to="/Seconnecter">Se connecter</RouterLink>
-    </li>
+    <li class="lg:hidden"><RouterLink class="text-light-gray p-4 block rounded-lg" to="/Seconnecter">Se connecter</RouterLink></li>
     <li class="flex justify-start items-center lg:hidden">
       <RouterLink to="/" class="mr-2">
         <drapeau/>
@@ -84,4 +76,5 @@ const menuIsOpen = ref(false)
 
     
   </header>
+</template>
 </template>
